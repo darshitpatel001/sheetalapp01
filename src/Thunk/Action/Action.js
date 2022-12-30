cd3031e0e8abc8198c8c332d51e042eda8aa74f6
darshitpatel001 <darshitakbari9999@gmail.com>
2022-12-30 18:52:38 +0600

    fdt

--- src/Thunk/Action/Action.js
+++ src/Thunk/Action/Action.js
@@ -1,20 +1,48 @@
 export const Plus = (paylode) => ({
-    type:"PLUS",
-    paylode
-})
+  type: "PLUS",
+  paylode,
+});
 
 export const Minus = (paylode) => ({
-    type:"MINUS",
-    paylode
-})
+  type: "MINUS",
+  paylode,
+});
 
-export const asncminus = num => {
-    return dispatch => {
-        setTimeout(() => {
-            dispatch({
-                type :"Minustunk",
-                paylode: num
-            })
-        }, 1000);
+export const getUserListStarted = () => ({
+    type: 'Started',
+    
+  })
+  
+  export const getUserListSuccess = (payload) => ({
+    type: 'GET_USER_SUC',
+    payload
+  })
+  
+  export const getUserListFailure = (payload) => ({
+    type: 'GET_USER_FAILED',
+    payload
+  })
+
+export const asncminus = (num) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch({
+        type: "Minustunk",
+        paylode: num,
+      });
+    }, 1000);
+  };
+};
+
+export const Userasnc =
+  (page = 1) =>
+  async (dispatch) => {
+    dispatch(getUserListStarted());
+    try {
+      const res = await fetch(`https://reqres.in/api/users?page=${page}`);
+      const data = await res.json();
+      dispatch(getUserListSuccess(data));
+    } catch (err) {
+      dispatch(getUserListFailure(err.message));
     }
-}
+  };
