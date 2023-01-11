53e9d69b35b185364cf832ed566c99edb748dac3
darshitpatel001 <darshitakbari9999@gmail.com>
2023-01-11 19:22:42 +0600

    d

---
+++ src/PureComponente/Compo1.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export default class Compo1 extends PureComponent {
+  static propTypes = {second: third}
+
+  render() {
+    return (
+      <div>Compo1</div>
+    )
+  }
+}
---
+++ src/PureComponente/Compo2.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+import React, { Component ,PureComponent } from 'react'
+
+export default class Compo2 extends PureComponent {
+  static propTypes = {second: third}
+
+  render() {
+    return (
+      <div>Compo2</div>
+    )
+  }
+}
