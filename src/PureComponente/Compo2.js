be3b7eac94a4a326c9c2b81cdc9c55bdbae02b96
darshitpatel001 <darshitakbari9999@gmail.com>
2023-01-12 17:43:04 +0600

    dgr

--- src/PureComponente/Compo2.js
+++ src/PureComponente/Compo2.js
@@ -2,7 +2,6 @@ import PropTypes from "prop-types";
 import React, { Component, PureComponent } from "react";
 
 export default class Compo2 extends PureComponent {
-  static propTypes = { second: third };
 
   render() {
     return <div>{alert("This is PureFunction")}</div>;
