b5247d174918e950c2a08471cc6da4d44bc5a7f6
darshitpatel001 <darshitakbari9999@gmail.com>
2023-01-17 19:08:23 +0600

    f

--- src/PureComponente/Formcom.js
+++ src/PureComponente/Formcom.js
@@ -5,7 +5,9 @@ export default class Formcom extends Component {
     state = {
         firstName : "",
         lastName : "",
-        Address : {}
+        Address : {
+          city:''
+        }
 
     }
 
@@ -21,11 +23,16 @@ export default class Formcom extends Component {
   render() {
     return (
       <div>
-
-            <input type="text" value={this.state.firstName} name="firstName" onChange={this.Handler} />
+        <form  class="w-50 mx-auto" onSubmit={this.mySubmit}>
+            <label htmlFor="" class="form-label"> First Name : </label>
+            <input value={this.state.lastName}  name="firstName" onChange={this.Handler} />
+            <label htmlFor="" class="form-label"> Last Name : </label>
             <input type="text" value={this.state.lastName}  name="lastName" onChange={this.Handler}/>
-            <input type="submit" value="save" />
-
+            <label htmlFor="" class="form-label"> Address : </label>
+            <label htmlFor="" class="form-label"> City : </label>
+            <input type="text" value={this.state.city}  name="city" onChange={this.Handler}/>
+            {/* <input type="submit" value="save" /> */}
+            <button class="form-control btn btn-outline-primary" type="button">Submit</button>
         </form>
       </div>
     )
